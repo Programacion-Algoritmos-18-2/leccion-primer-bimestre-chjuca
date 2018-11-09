6a3cfd7f7d3962244995ab0c6001a4cc785d5968
Carlos Juca <chjuca@utpl.edu.ec>
2018-11-09 11:51:41 -0500

    Agregando Segundo commit

--- ejercicio1/mipaquete/modelado.py
+++ ejercicio1/mipaquete/modelado.py
@@ -1,4 +1,4 @@
-class Empleado():								# Creamos la Clase Empleado
+class Empleado(object):								# Creamos la Clase Empleado
 
 	def __init__(self):							# Creamos el Metodo __init__
 		self.nombre = ""						# Atributos de la clase Empleado
@@ -29,10 +29,87 @@ class Empleado():								# Creamos la Clase Empleado
 
 	def obtenerCedula(self):					#Esta funcion devuelve el atributo cedula
 		return self.cedula
-		
+
 	def presentarDatos(self):
-		cadena="Informacion de %s %s\n\t CEDULA: %s"%(self.obtenerNombre(),self.apellido,self.obtenerCedula())
+		cadena = "Informacion de %s %s\n\t CEDULA: %s" % (self.obtenerNombre(),self.apellido,self.obtenerCedula())
 		return cadena
 
 
+class EmpleadoPorHoras(Empleado):				# Creamos la Clase EmpleadoporHoras hijo de Empleados
+
+	def __init__(self):								# Creamos el Metodo __init__
+		super(EmpleadoPorHoras, self).__init__()	# Herencia de Atributos de la clase Empleado
+		self.numeroHoras = 0						# Atributos de la clase EmpleadoporHoras
+		self.valorHora = 0.0
+
+	def agregarNumeroHoras(self,horas):				#Funcion da valor horas el atributo numeroHoras		
+		self.numeroHoras=horas
+
+	def obtenerNumeroHoras(self):					#Esta funcion devuelve el atributo numeroHoras
+		return self.numeroHoras
+
+	def agregarValorHora(self,valor):				#Funcion da valor el atributo valorHoras		
+		self.valorHora = valor
+
+	def obtenerValorHora(self):						#Esta funcion devuelve el atributo valorHora
+		return self.valorHora
+
+	def calcularValorSueldoFinal(self):				#Esta Funcion Calcula el sueldo final que recibra el Empleado
+		sueldoFinal = (self.numeroHoras*self.valorHora)+self.obtenerComisionFija()
+		return sueldoFinal
+
+	def presentarDatos(self):						# Presentamos Datos
+		cadena = "%s\nNumero Horas: %d\nValor Hora: %.2f\nSueldo Final: %.2f" % (super(EmpleadoPorHoras,self).presentarDatos(),self.obtenerNumeroHoras(),self.obtenerValorHora(),self.calcularValorSueldoFinal())
+		return cadena	
+
+class EmpleadoFijo(Empleado):						# Creamos la Clase EmpleadoFijo hijo de Empleados
+
+	def __init__(self):								# Creamos el Metodo __init__
+		super(EmpleadoFijo, self).__init__()		# Herencia de Atributos de la clase Empleado
+		self.sueldoFijo = 0							# Atributos de la clase EmpleadoFijo
+		self.descuentoSeguro = 0
+
+	def agregarSueldoFijo(self,sueldo):				#Funcion da valor sueldo el atributo sueldoFijo	
+		self.sueldoFijo = sueldo
+
+	def obtenerSueldoFijo(self):					#Esta funcion devuelve el atributo sueldoFijo	
+		return self.sueldoFijo
+	
+	def agregarDescuentoSeguro(self,desc):			#Funcion da valor sueldo el atributo descuentoSeguro
+		self.descuentoSeguro = desc
+
+	def obtenerDescuentoSeguro(self):				#Esta funcion devuelve el atributo descuentoSeguro
+		return self.descuentoSeguro
+
+	def calcularSueldoFinal(self):					# Calculamos el SueldoFinal
+		return (self.obtenerSueldoFijo()*(100-self.obtenerDescuentoSeguro())) + self.obtenerComisionFija()
+
+	def presentarDatos(self):						# Presentamos Datos
+		cadena = ("%s\nSueldo Fijo: %.2f\nDescuento Seguro: %.2f\nSueldo Final: %.2f\n" % (super(EmpleadoFijo, self).presentarDatos(), self.obtenerSueldoFijo(), self.obtenerDescuentoSeguro(), self.calcularSueldoFinal()))
+		return cadena
+
+class EmpleadoPorSemana(Empleado):					# Creamos la Clase EmpleadoPorSemana hijo de Empleados
 		
+	def __init__(self):								# Creamos el Metodo __init__
+		super(EmpleadoPorSemana, self).__init__()	# Herencia de Atributos de la clase Empleado
+		self.numeroSemanas = 0						# Atributos de la clase EmpleadoPorSemana
+		self.valorSemanal = 0
+
+	def agregarNumeroSemanas(self, semanas):		#Funcion da valor ssemanas el atributo numeroSemanas
+		self.numeroSemanas = semanas
+
+	def obtenerNumeroSemanas(self):					#Esta funcion devuelve el atributo numeroSemanas
+		return numeroSemanas
+
+	def agregarValorSemanal(self, valor):			#Funcion da valor el atributo valorSemanal
+		self.valorSemanal = valor
+
+	def obtenerValorSemanal(self):					#Esta funcion devuelve el atributo valorSemanal
+		return valorSemanal
+
+	def calcularSueldoFinal(self):					# Calcula el sueldo Final
+		return self.obtenerNumeroSemanas() * self.obtenerValorSemanal() + self.obtenerComisionFija()
+
+	def presentarDatos(self):						# Se presenta Datos
+		cadena = ("%s\nNumero Semanas: %.2f\nValor semanal: %.2f\nSueldo final: %.2f\n"  % (super(EmpleadoPorSemana, self).presentarDatos(), self.obtenerNumeroSemanas(), self.obtenerValorSemanal(), self.calcularSueldoFinal()))
+		return cadena
--- ejercicio1/run.py
+++ ejercicio1/run.py
@@ -6,3 +6,22 @@ e.agregarNombre("Luis")					# Llamamos a la funcion agregarNombre con el paramet
 e.agregarApellido("Benitez")			# Llamamos a la funcion agregarApellido con el parametro "Benitez"
 e.agregarCedula("1110001")				# Llamamos a la funcion agregarCedula con el parametro "1110001"
 print(e.presentarDatos())				# Llamamos a la funcion presentarDatos
+
+e1 = EmpleadoPorHoras()					# Creamos el Objeto para la Clase EmpleadoPorHoras
+nombre=input("Ingrese su nombre: ")		# Se lee por teclado el Nombre
+e1.agregarNombre(nombre)				# Se le da valores a los atributos
+e1.agregarApellido("Andrade")
+e1.agregarCedula("112233")
+e1.agregarNumeroHoras(15)
+e1.agregarValorHora(20.2)
+print(e1.presentarDatos())				# Se presenta Datos
+
+e2=EmpleadoFijo()						# Creamos el Objeto para la Clase EmpleadoFijo
+e2.agregarSueldoFijo(300.2)				#Se le da valor a los atributos
+e2.descuentoSeguro = 10
+comision = input("Ingrese Comision: ")	# Se lee por teclado el valor de la comision
+comision = float(comision)				# Se convierte comision en float
+e2.agregarComisionFija(comision)		# Se le da valor a los atributos
+e2.nombre = "Ana"
+e2.apellido = "Diaz"
+print(e2.presentarDatos())
